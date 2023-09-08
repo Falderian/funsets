e256a52e7e1fa87eb02142e40bd736c1aec8227c
Nikita Staselovich <nikitastaselovich@gmail.com>
2023-09-08 10:47:36 +0300

    UPD: finished assignment

--- src/main/scala/funsets/FunSets.scala
+++ src/main/scala/funsets/FunSets.scala
@@ -68,7 +68,7 @@ trait FunSets extends FunSetsInterface:
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: FunSet, f: Int => Int): FunSet = ???
+  def map(s: FunSet, f: Int => Int): FunSet = (x => exists(s, a=> f(a) == x))
 
   /**
    * Displays the contents of a set
